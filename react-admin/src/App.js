7549a1608adc7d93d6dfd622f045f8ce237f74a7
wsz <wsz007007@163.com>
2019-06-18 22:26:56 +0800

    06.18 init

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import {Button} from 'antd'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button >antd按钮</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
