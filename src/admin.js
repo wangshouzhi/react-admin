f57f0c1c7f4e5f2083971e5762fdc9787c692ec0
wangshouzhi <wangshouzhi8008@163.com>
2019-06-19 10:29:55 +0800

    06.19"

---
+++ src/admin.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import { Row } from 'antd'
+
+export default class Admin extends Component {
+    render() {
+        return(
+            <Row>
+                <Col span='3'></Col>
+                <Col span='21'></Col>
+            </Row>
+        )
+    }
+}
